169e3b51061e3e86d533a1b0de06dbc80818e751
Muhammad Usman Akbar <45659899+muhamadusman@users.noreply.github.com>
2023-06-30 11:15:12 +0200

    Update ReadMe.py

--- ProgressiveGAN/ReadMe.py
+++ ProgressiveGAN/ReadMe.py
@@ -1,9 +1,11 @@
 Based on using the progressive growing GAN by Nvidida, https://github.com/tkarras/progressive_growing_of_gans 
 
+this repository uses tfRecords to train 
+
 Use .yml file to make conda environment 
 
-Modify config.py in the PGGAN code to use your new dataset.
+Modify config.py in the PGGAN code to use your dataset
 
-Run train.py in inetiate training. 
+Run train.py in initiate training. 
 
 Warning: This code does not work with too new graphics cards, like RTX 3090 from the Ampere architecture, as the code is using TF 1.14.
