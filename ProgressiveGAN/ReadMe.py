89a51fedb73ee1fdf7f7fb26a94854c4a90b6c98
Muhammad Usman Akbar <45659899+muhamadusman@users.noreply.github.com>
2023-06-30 10:25:00 +0200

    Create ReadMe.py

---
+++ ProgressiveGAN/ReadMe.py
@@ -0,0 +1,9 @@
+Based on using the progressive growing GAN by Nvidida, https://github.com/tkarras/progressive_growing_of_gans 
+
+Use .yml file to make conda environment 
+
+Modify config.py in the PGGAN code to use your new dataset.
+
+Change train.py
+
+Run train.py in inetiate training. 
