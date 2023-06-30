da7ee1667bd53f6d279119b7a7b0492fd36363cf
Muhammad Usman Akbar <45659899+muhamadusman@users.noreply.github.com>
2023-06-30 11:06:46 +0200

    Update ReadMe.py

--- ProgressiveGAN/ReadMe.py
+++ ProgressiveGAN/ReadMe.py
@@ -5,3 +5,5 @@ Use .yml file to make conda environment
 Modify config.py in the PGGAN code to use your new dataset.
 
 Run train.py in inetiate training. 
+
+Warning: This code does not work with too new graphics cards, like RTX 3090 from the Ampere architecture, as the code is using TF 1.14.
